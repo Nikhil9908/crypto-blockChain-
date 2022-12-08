6924934db21ce5452b290cc4628d2430e4d69bd4
Nikhil9908 <imnikhilmishra@gmail.com>
2022-12-08 23:59:50 +0530

    Headers added

--- src/coinController/coinData.js
+++ src/coinController/coinData.js
@@ -1,12 +1,14 @@
 const coinModel = require('../coinModel/coinModel')
 const axios = require('axios')
-const { findOneAndUpdate } = require('../coinModel/coinModel')
 
 const coinData = async function(req, res){
     try {
         let options = {
             method : 'get',
             url : 'https://api.coincap.io/v2/assets',
+            Headers:{
+                 Authorization: "Bearer 2ff81000-15bf-42fc-b172-48d9b5a8c88d"
+            }
         }
         let result = await axios(options)
         console.log(result);
@@ -15,7 +17,7 @@ const coinData = async function(req, res){
         let sortedData = data.sort((a,b)=>{b.changePercent24Hr - a.changePercent24Hr})
            await coinModel.deleteMany()
            let finalData = await coinModel.create(sortedData)
-      return res.status(200).send({status: true , msg: finalData})
+      return res.status(200).send({status: true , data: finalData})
     } catch (error) {
         console.log(error);
         res.status(500).send({message: error.message})
